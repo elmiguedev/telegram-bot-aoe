9b072aaff5b657b0d52c89158b96151e16e4445b
name <mmoyano16@gmail.com>
2018-11-03 19:31:50 -0300

    Se agregan audios en ingles

--- server.js
+++ server.js
@@ -6,12 +6,21 @@ const Telebot = require('telebot');
 const bot = new Telebot(process.env.TOKEN);
 const audios = require('./audios.json');
 
+const tauntsCount = 42;
+
 // recorre todas las posibilidades del array
-for(let i = 0; i < audios.length; i++) {
+for(let i = 0; i <= tauntsCount; i++) {
   
   // por cada item, agrega un handler
   bot.on(`/a${i+1}`,function(msg) {
-    return bot.sendVoice(msg.chat.id, audios[i].audio);
+    //return bot.sendVoice(msg.chat.id, audios[i].audio);
+    return bot.sendVoice(msg.chat.id, `./audios/spanish/${i+1}.opus`);
+  }); 
+  bot.on(`/a${i+1}s`,function(msg) {
+    return bot.sendVoice(msg.chat.id, `./audios/spanish/${i+1}.opus`);
+  }); 
+  bot.on(`/a${i+1}e`,function(msg) {
+    return bot.sendVoice(msg.chat.id, `./audios/english/${i+1}.opus`);
   }); 
  
 }
